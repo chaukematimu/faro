a211aa12b28fe5396957720a566cb076695c9e68
Matimu Chauke <matimu@chauke.org>
2024-07-22 05:17:01 +0200

    - Rename fields to be readable - Add ID Number validation

--- faro/faro_selling/custom/customer.py
+++ faro/faro_selling/custom/customer.py
@@ -1,11 +1,34 @@
 import logging
+from typing import TYPE_CHECKING
 
+from frappe import _
+import frappe
 from erpnext.selling.doctype.customer.customer import Customer as ERPNextCustomer
+from frappe.types import DF
 
 logger = logging.getLogger(__name__)
 
 
 class Customer(ERPNextCustomer):
+    if TYPE_CHECKING:
+        is_passport_or_id_number: DF.Literal["ID Number", "Passport"]
+        passport_or_id_number: DF.Data
+        passport_country: DF.Data
 
     def validate(self):
+        if self.is_passport_or_id_number == "ID Number":
+            self.validate_id_number()
         super().validate()
+
+    def validate_id_number(self):
+        rules = [
+            self.passport_or_id_number.isdigit(),
+            len(self.passport_or_id_number) == 13
+            # TODO : validate ID using Luhn algorithm
+        ]
+        if not all(rules):
+            frappe.throw(
+                _(
+                    "Invalid ID Number"
+                )
+            )
